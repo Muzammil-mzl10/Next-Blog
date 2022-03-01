8de0faf90af331a91660483ba5fa334b68bad40c
Muzammil Ahmad <56504584+Muzammil786-beep@users.noreply.github.com>
2022-03-01 17:17:40 +0500

    second

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,8 +1,32 @@
 import react from "react";
-
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 const Blog = ()=>{
     return(<>
+    <div className={styles.container}>
+    <main className={styles.main}>
         <h1>This is React Blog</h1>
+        <div className="blogs">
+            <h2> Latest Blogs</h2>
+            <div className="blogItems">
+            <Link href={'/blogpost/learn-javascript'}>
+                <a >        <h3>How to Learn Javascript in 2022?</h3>
+              <p>Javascript is used to design Backend for the website</p></a>
+            </Link>
+
+      
+            </div>
+            <div className="blogItems">
+              <h3>How to Learn Javascript in 2022? </h3>
+              <p>Javascript is used to design Backend for the website</p>
+            </div>
+            <div className="blogItems">
+              <h3>How to Learn Javascript in 2022? </h3>
+              <p>Javascript is used to design Backend for the website</p>
+            </div>
+            </div>
+            </main>
+            </div>
     </>)
 }
 export default Blog;
--- pages/blogpost/[slug].jsx
+++ pages/blogpost/[slug].jsx
@@ -1,10 +1,19 @@
 import react from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/Blogpost.module.css";
+
 const Slug = ()=>{
     const router = useRouter();
       const {slug} =  router.query;
     return(<>
-        <h1>This is React {slug}</h1>
+    <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+
+        <div className={styles.container}>
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut repellendus, nam magnam dolores libero blanditiis repudiandae asperiores eaque, aperiam tenetur similique praesentium cumque, tempora quibusdam sint in! Fuga ea fugit consectetur cupiditate impedit nam exercitationem animi necessitatibus et. Aliquam voluptas voluptatum quo officiis sequi placeat!
+        </div>
+        </main>
     </>)
 }
 export default Slug;
